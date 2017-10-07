e411afd63fc249d8be935f0a9db38d17b894de8b
Naga Muralidhar Sri Ram Boddapati <nagamuralidharsriramboddapati@Nagas-MacBook-Air.local>
2017-10-07 15:18:27 -0700

    chatteting

--- client/src/app/components/bot/chat.component.ts
+++ client/src/app/components/bot/chat.component.ts
@@ -17,13 +17,17 @@ export class ChatComponent {
   conversationString: string = '<li class="right clearfix"><span class="chat-img pull-right"><img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><small class=" text-muted"><span class="glyphicon glyphicon-time"></span>Donna @ The Gentlemens Barbershop </small><strong class="pull-right primary-font">Donna</strong></div><p> ' + this.donnaReply + '</p></div></li>';
 
   constructor(private chatService:ChatService) {
-    console.log("Chat component rendering");
+    console.log("Chat component working");
     this.chatService.connect();
 
     this.chatService.socket.on('donnaReply', (message) => {
       console.log(message);
       this.donnaReplies(message);
     })
+  }
+  sendTestMsg(msg){
+    this.chatService.sendMessage(msg);
+    
   }
 
   sendMessage(box){
